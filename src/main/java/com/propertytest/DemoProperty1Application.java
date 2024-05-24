ff8382280405cacf3aabc848862dba38c77c9ce4
chakradhar <chakradhar@metamorphtech.com>
2024-05-24 12:53:03 +0530

    fix

---
+++ src/main/java/com/propertytest/DemoProperty1Application.java
@@ -0,0 +1,13 @@
+package com.propertytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProperty1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProperty1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/propertytest/state/StateName.java
@@ -0,0 +1,11 @@
+package com.propertytest.state;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StateName {
+
+	public String currentStateName() {
+		return "MH";
+	}
+}
